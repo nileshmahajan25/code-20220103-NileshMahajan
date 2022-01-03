774ff6ece38e55f3f458b7406df9ac852071129b
Nilesh Mahajan <nileshmahajan49@gmail.com>
2022-01-03 09:25:14 +0530

    Relative path

--- BMICal/test_BMICal.py
+++ BMICal/test_BMICal.py
@@ -9,7 +9,7 @@ class test_BMICal(unittest.TestCase):
         inputData = '[{"Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, { "Gender": "Male", "HeightCm": 161, "WeightKg": 85 }, { "Gender": "Male", "HeightCm": 180, "WeightKg": 77 }, { "Gender": "Female", "HeightCm": 166, "WeightKg": 62}, {"Gender": "Female", "HeightCm": 150, "WeightKg": 70}, {"Gender": "Female", "HeightCm": 167, "WeightKg": 82}]'
         jsonData = json.loads(inputData)  # Convert input string data into json format
         result = BMICal.bmiCalculator(jsonData)
-        expectedResult = pd.read_csv('bmical_testresult.csv')
+        expectedResult = pd.read_csv('./bmical_testresult.csv')
         assert_frame_equal(expectedResult, result)
 
 if __name__ == '__main__':
