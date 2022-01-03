c52aef69efe6b322c4e66c5e05d03a2040ccd3d8
Nilesh Mahajan <nileshmahajan49@gmail.com>
2022-01-03 09:34:52 +0530

    provided absolute path for test file

--- BMICal/test_BMICal.py
+++ BMICal/test_BMICal.py
@@ -3,13 +3,17 @@ import BMICal
 import json
 from pandas.testing import assert_frame_equal
 import pandas as pd
+import os
 
 class test_BMICal(unittest.TestCase):
     def test_bmiCalculator(self):
+        testFileName = 'bmical_testresult.csv'
         inputData = '[{"Gender": "Male", "HeightCm": 171, "WeightKg": 96 }, { "Gender": "Male", "HeightCm": 161, "WeightKg": 85 }, { "Gender": "Male", "HeightCm": 180, "WeightKg": 77 }, { "Gender": "Female", "HeightCm": 166, "WeightKg": 62}, {"Gender": "Female", "HeightCm": 150, "WeightKg": 70}, {"Gender": "Female", "HeightCm": 167, "WeightKg": 82}]'
         jsonData = json.loads(inputData)  # Convert input string data into json format
         result = BMICal.bmiCalculator(jsonData)
-        expectedResult = pd.read_csv('./bmical_testresult.csv')
+
+        absTestFilePath = os.path.abspath(testFileName)
+        expectedResult = pd.read_csv(absTestFilePath)
         assert_frame_equal(expectedResult, result)
 
 if __name__ == '__main__':
